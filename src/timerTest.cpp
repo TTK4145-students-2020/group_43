b4c3fe0d1807f69345a47182764f401ce84b9396
viki <endre.t.e@hotmail.com>
2020-03-28 12:39:03 -0500

    Switched to uppercase names. Added timerThread, a general timer, does not need an order. Also added resetTimer() to both timer modules.

--- src/timerTest.cpp
+++ src/timerTest.cpp
@@ -1,13 +1,12 @@
 
 #include <stdio.h>
 #include <unistd.h>
-
 #include "globals.hpp"
-#include "order_timer.hpp"
+#include "orderTimer.hpp"
 
 int main(){
     char c = 0;
-    double duration = 4;
+    double duration = 3;
     
     while(1){
         printf("\ninitate timer? press y then enter:");
@@ -15,20 +14,24 @@ int main(){
         {
             scanf("%c", &c);
         }
-        order myOrder = { .data=0};
-
-        order_timer myTimer(&myOrder, duration);            //Starts timer
-        printf("\ntimeout: %i", myTimer.is_timed_out());    //Check flag
-        for (int i = 0; i < 5; i++)
+        order myOrder = { .data = 0};
+        orderTimer myTimer(&myOrder, duration);               //Starts timer
+        printf("\ntimeout: %i", myTimer.isTimedOut());    //Check flag
+        int i = 0;
+        while (!myTimer.isTimedOut())
         {
-            printf("\n main thread sleeping ..  \n");
-            sleep(1);
-            if (i==3)
+            //printf("timeout: %i", myTimer.isTimedOut());
+            if (i==2)
             {
-                //myTimer.stop_timer();     //Stops timer.
+                printf("\nResetting timer");
+                myTimer.resetTimer();
             }
+            printf("\nmain thread sleeping ..");
+            sleep(1);
+            i++;
         }
-        printf("\ntimeout: %i", myTimer.is_timed_out());
+        
+        printf("\ntimeout: %i", myTimer.isTimedOut());
         c = 'n';
     }
     return 0;
